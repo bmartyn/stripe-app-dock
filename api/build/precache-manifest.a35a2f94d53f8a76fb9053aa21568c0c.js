8f627cf7b539834c36e144bb5c57d9eb209c5620
Rishi <hrushikeshchapke@gmail.com>
2022-07-15 23:01:58 +0530

    added build folder

---
+++ api/build/precache-manifest.a35a2f94d53f8a76fb9053aa21568c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc640bcb1650d1e1d9d5c260139f0e95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f6146efa383acb2897",
+    "url": "/static/css/2.da393fb9.chunk.css"
+  },
+  {
+    "revision": "10794ffdfc2643980166",
+    "url": "/static/css/main.7734cfe9.chunk.css"
+  },
+  {
+    "revision": "00f6146efa383acb2897",
+    "url": "/static/js/2.07648ddf.chunk.js"
+  },
+  {
+    "revision": "10794ffdfc2643980166",
+    "url": "/static/js/main.c743a13d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
